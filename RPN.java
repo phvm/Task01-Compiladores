07cffb74165e33db1b827b3b338ef7e5a2c05bf5
PedroMouraTI <48687882+phvm@users.noreply.github.com>
2021-11-05 14:05:43 -0300

    Atualizada para ler arquivos

--- RPN.java
+++ RPN.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -7,21 +9,23 @@ public class RPN {
     public RPN(){
             pilha = new Stack<Integer>();
             }
-    public static void main(String[] args){
-            String expressao;
-            int resultado;
-
-            Scanner teclado = new Scanner(System.in);
-
-            RPN avaliador = new RPN();
-            expressao = teclado.nextLine();
-
-            resultado = avaliador.avaliar(expressao);
-            System.out.println(resultado);
-       }
-
-    public int avaliar(String expr) 
-        {
+    public static void main(String[] args) throws Exception{
+        String expressao;
+        int resultado;
+        RPN avaliador = new RPN();
+        expressao = "";
+        //alterar o path para o do arquivo que possuir a expressao a ser analisada. 
+        FileReader fr = new FileReader("C:\\Users\\user\\Desktop\\Testes\\Compiladores\\src\\Calc1.stk");
+        BufferedReader lerArq = new BufferedReader(fr);
+        String linha = lerArq.readLine();
+        while (linha != null) {
+            expressao += linha + " ";
+            linha = lerArq.readLine(); //
+          }
+        resultado = avaliador.avaliar(expressao);
+        System.out.println(resultado);
+   }
+	public int avaliar(String expr){
             int op1, op2, resultado = 0;
             String token;
             Scanner in = new Scanner(expr);
@@ -44,14 +48,12 @@ public class RPN {
             return resultado;
         }
 
-     private boolean isOperador(String token)
-        {
+     private boolean isOperador(String token){
             return ( token.equals("+") || token.equals("-") ||
                      token.equals("*") || token.equals("/") || token.equals("%") );
 
         }
-     private int avaliarOperador(char operacao, int op1, int op2)
-        {
+     private int avaliarOperador(char operacao, int op1, int op2){
             int resultado = 0;
 
             switch (operacao)
